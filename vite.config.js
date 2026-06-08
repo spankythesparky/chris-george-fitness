6ffc1a3cab98b6ed044baca5133edbfe32360c76
Owner <Owner@MacBookAir.lan>
2026-06-08 00:48:37 -0400

    Per-page link previews

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        services: resolve(__dirname, "services.html"),
+        apparel: resolve(__dirname, "apparel.html"),
+        supplements: resolve(__dirname, "supplements.html"),
+        contact: resolve(__dirname, "contact.html"),
+      },
+    },
+  },
 });
